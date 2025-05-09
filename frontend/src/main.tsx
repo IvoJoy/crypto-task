dd4b23c86ebfca556f9034ee9b31b3ab2b9b9093
IvoJoy <ivkata1112@abv.bg>
2025-05-10 00:48:27 +0300

    Creating the frontend skeleton

---
+++ frontend/src/main.tsx
@@ -0,0 +1,81 @@
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  Outlet,
+  RouterProvider,
+  createRootRoute,
+  createRoute,
+  createRouter,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import DemoTable from './routes/demo.table.tsx'
+import DemoTanstackQuery from './routes/demo.tanstack-query.tsx'
+
+import Header from './components/Header.tsx'
+
+import TanstackQueryLayout from './integrations/tanstack-query/layout.tsx'
+
+import * as TanstackQuery from './integrations/tanstack-query/root-provider.tsx'
+
+import './styles.css'
+import reportWebVitals from './reportWebVitals.ts'
+
+import App from './App.tsx'
+
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <Header />
+      <Outlet />
+      <TanStackRouterDevtools />
+
+      <TanstackQueryLayout />
+    </>
+  ),
+})
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: App,
+})
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  DemoTable(rootRoute),
+  DemoTanstackQuery(rootRoute),
+])
+
+const router = createRouter({
+  routeTree,
+  context: {
+    ...TanstackQuery.getContext(),
+  },
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('app')
+if (rootElement && !rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <TanstackQuery.Provider>
+        <RouterProvider router={router} />
+      </TanstackQuery.Provider>
+    </StrictMode>,
+  )
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
